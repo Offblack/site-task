ec4404584f0aa3231a553fb6ff9886a4a653c61c
Michal Opalka <michalopalka998@gmail.com>
2019-10-07 17:04:58 +0200

    Initial develop commit

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,12 @@
-import React from "react"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React from "react";
+import SEO from "../components/seo";
 
 const NotFoundPage = () => (
-  <Layout>
+  <>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+    <h1>404 - Ops, not found</h1>
+    <p>Przykro nam, ta strona nie istnieje.</p>
+  </>
+);
 
-export default NotFoundPage
+export default NotFoundPage;
