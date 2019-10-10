f1abeff58818959c57fd38772c8b13b1a1ab93a5
Michal Opalka <michalopalka998@gmail.com>
2019-10-10 23:31:35 +0200

    Changed margin after Header

--- src/components/atoms/Header/index.js
+++ src/components/atoms/Header/index.js
@@ -3,14 +3,15 @@ import styled from 'styled-components';
 const Header = styled.h1`
   font-size: 24px;
   font-weight: 400;
-  margin: 50px auto 70px;
+  margin: 50px auto 42px;
+  padding: 0;
 
   ::after {
     content: '';
     display: block;
     width: 37px;
     height: 2px;
-    margin: 18px auto;
+    margin: 18px auto 0px;
     /* Background gradients */
     background: rgb(241, 0, 125);
     background: linear-gradient(
