6c1601d27fbae0e4ba8a5dba04588a42d43fc736
Michal Opalka <michalopalka998@gmail.com>
2019-10-10 23:25:05 +0200

    Added gradient transition on hover

--- src/components/molecules/NewsItem/index.js
+++ src/components/molecules/NewsItem/index.js
@@ -3,24 +3,39 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const StyledBackground = styled.div`
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 240px;
   height: 319px;
   background: transparent;
-  background: rgba(255, 255, 255, 0);
+  background-image: rgba(255, 255, 255, 0);
   border-radius: 7px;
   cursor: pointer;
 
-  :hover {
-    /* Background gradients*/
+  ::before {
+    position: absolute;
+    content: '';
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    border-radius: 7px;
     background: rgb(241, 0, 125);
-    background: linear-gradient(
+    background-image: linear-gradient(
       45deg,
       rgba(241, 0, 125, 1) 0%,
       rgba(0, 158, 226, 1) 100%
     );
+    box-shadow: 10px 10px 20px 0px rgba(0, 0, 0, 0.1);
+    z-index: -1;
+    transition: opacity 0.4s ease-in;
+    opacity: 0;
+  }
+
+  :hover::before {
+    opacity: 1;
   }
 `;
 
